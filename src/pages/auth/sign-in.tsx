d2ed78a147e040965cf823c12c632cae30197abb
Stévan Corre <stevancorre@protonmail.com>
2022-09-09 09:39:21 +0200

    [auth] implement loading ui

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -19,8 +19,11 @@ const SignInPage: NextPage = () => {
     const { errors } = formState;
 
     const [authError, setError] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
 
-    const onSubmit = (credentials: SignInInput) =>
+    const onSubmit = (credentials: SignInInput) => {
+        if (isLoading) return;
+        setIsLoading(true);
         signIn("credentials", {
             ...credentials,
             redirect: false,
@@ -30,10 +33,13 @@ const SignInPage: NextPage = () => {
                 if (res.status === 401) setError("Indentifiants invalides");
                 else setError(res.error);
             else if (res?.url) await router.push(res.url);
+
+            setIsLoading(false);
         });
+    };
 
     useEffect(() => {
-        if (session.data?.user?.email) router.push("/");
+        if (session.data?.user) router.push("/");
     }, [session, router]);
 
     return (
@@ -83,6 +89,7 @@ const SignInPage: NextPage = () => {
                                     <input
                                         id="email"
                                         placeholder="utilisateur@groupomania.com"
+                                        disabled={isLoading}
                                         className={`block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-zinc-800 sm:text-sm ${
                                             errors.email || authError
                                                 ? "border-red-500 focus:outline-red-500"
@@ -105,6 +112,7 @@ const SignInPage: NextPage = () => {
                                         id="password"
                                         type="password"
                                         placeholder="••••••••"
+                                        disabled={isLoading}
                                         className={`block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-zinc-800 focus:border-blue-500 focus:ring-blue-500 sm:text-sm ${
                                             errors.password || authError
                                                 ? "border-red-500 focus:outline-red-500"
@@ -118,14 +126,34 @@ const SignInPage: NextPage = () => {
                                 </div>
                                 <button
                                     type="submit"
+                                    disabled={isLoading}
                                     className="w-full rounded-lg px-5 py-2.5 text-center text-sm font-medium text-zinc-800 focus:outline-none focus:ring-4"
                                 >
-                                    Se connecter
+                                    {isLoading ? (
+                                        <svg
+                                            role="status"
+                                            className="mr-2 inline h-4 w-4 animate-spin text-gray-200 dark:text-gray-600"
+                                            viewBox="0 0 100 101"
+                                            fill="none"
+                                            xmlns="http://www.w3.org/2000/svg"
+                                        >
+                                            <path
+                                                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                                                fill="currentColor"
+                                            />
+                                            <path
+                                                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                                                fill="#4e5166"
+                                            />
+                                        </svg>
+                                    ) : (
+                                        "Se connecter"
+                                    )}
                                 </button>
                                 <p className="text-sm font-light text-zinc-600">
                                     Pas encore de compte ?{" "}
                                     <Link href="/auth/sign-up">
-                                        <span className="cursor-pointer font-medium text-blue-600 hover:underline">
+                                        <span className="animate-spin cursor-pointer font-medium text-blue-600 hover:underline">
                                             Créer un compte
                                         </span>
                                     </Link>
